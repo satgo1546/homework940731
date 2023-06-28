b74c4732577f32d79baa208bb9a0fec2d2b7abc8
Frog Chen <satgo1546@yeah.net>
2023-06-28 11:27:11 +0800

    参考element-starter引入Element UI

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-button @click="startHacking">alert</el-button>
+  </div>
+</template>
+
+<script setup>
+function startHacking() {
+  alert(1)
+}
+</script>
+
+<style>
+</style>
