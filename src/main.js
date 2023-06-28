a88048874b5eff3f45cdc18ec083ced555c1a797
Frog Chen <satgo1546@yeah.net>
2023-06-28 17:21:47 +0800

    重命名App为Admin

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import App from './App.vue'
+import Admin from './Admin.vue'
 
 Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Admin)
 })
