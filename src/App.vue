6aa22febea35a9c892d2478f550c4094500c92ac
Frog Chen <satgo1546@yeah.net>
2023-06-28 17:14:26 +0800

    添加置顶功能

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,31 @@
         <el-table-column label="ID" prop="id" width="40"></el-table-column>
         <el-table-column label="图片" width="140">
           <template v-slot="{ row, $index }">
-            <img :src="'http://localhost:40731/' + row.filename" width="100" height="100" @click="dialogState.visible = true, dialogState.index = $index">
+            <img :src="'http://localhost:40731/' + row.filename" width="100" height="100"
+              @click="dialogState.visible = true, dialogState.index = $index">
           </template>
         </el-table-column>
         <el-table-column label="排序顺序" prop="order_num"></el-table-column>
         <el-table-column label="操作" width="144">
           <template v-slot="{ row }">
-            <el-checkbox v-model="row.visible" @change="updateRecord({ id: row.id, visible: row.visible })" label="审核通过" border></el-checkbox>
+            <el-checkbox v-model="row.visible" @change="updateRecord({ id: row.id, visible: row.visible })" label="审核通过"
+              border></el-checkbox>
             <el-button type="danger" icon="el-icon-delete" plain @click="removeRecord(row.id)">删除</el-button>
+            <el-button @click="updateRecord({ id: row.id, order_num: state.firstOrderNum /= 2 }), fetchData()">置顶</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-table-draggable>
     <el-pagination layout="prev, pager, next" :total="state.total" :page-size="state.count" :current-page="state.page + 1"
       @current-change="handleCurrentChange" />
-    <el-dialog
-      :title="'图片 ID ' + state.rows[dialogState.index].id"
-      :visible.sync="dialogState.visible"
-      width="95%">
+    <el-dialog :title="'图片 ID ' + state.rows[dialogState.index].id" :visible.sync="dialogState.visible" width="95%">
       <img :src="'http://localhost:40731/' + state.rows[dialogState.index].filename" style="max-width: 100%;">
       <span slot="footer" class="dialog-footer">
         <el-tag type="info">{{ dialogState.index + 1 }} / {{ state.rows.length }}</el-tag>
         <el-button @click="dialogState.index--" :disabled="dialogState.index === 0">上一张</el-button>
-        <el-checkbox v-model="state.rows[dialogState.index].visible" @change="updateRecord({ id: state.rows[dialogState.index].id, visible: state.rows[dialogState.index].visible })" label="审核通过" border></el-checkbox>
+        <el-checkbox v-model="state.rows[dialogState.index].visible"
+          @change="updateRecord({ id: state.rows[dialogState.index].id, visible: state.rows[dialogState.index].visible })"
+          label="审核通过" border></el-checkbox>
         <el-button @click="dialogState.index++" :disabled="dialogState.index === state.rows.length - 1">下一张</el-button>
       </span>
     </el-dialog>
@@ -46,6 +48,7 @@ const state = reactive({
   count: 10,
   prevLastOrderNum: 0,
   nextFirstOrderNum: 0,
+  firstOrderNum: 0,
 })
 
 const dialogState = reactive({
@@ -60,6 +63,7 @@ async function fetchData() {
       count: state.count + 2
     }
   })
+  if (state.page === 0) state.firstOrderNum = response.data.rows[0].order_num
   state.prevLastOrderNum = state.page === 0 ? 0 : response.data.rows[0].order_num
   state.nextFirstOrderNum = state.page === Math.ceil(response.data.total / state.count) - 1
     ? 114514.1919810 : response.data.rows[response.data.rows.length - 1].order_num
@@ -89,7 +93,7 @@ async function updateRecord(params) {
 }
 
 async function removeRecord(id) {
-  await axios.get('http://localhost:40731/delete', { params: {id} })
+  await axios.get('http://localhost:40731/delete', { params: { id } })
   await fetchData()
 }
 
